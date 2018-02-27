1ad4df41aaf6b4f1c36867acda60271f1c1e75e3
Aaron Caldwell <aaronjcaldwell@gmail.com>
2018-02-26 19:19:30 -0700

    Use babel loader for ES6

--- webpack.config.js
+++ webpack.config.js
@@ -9,5 +9,19 @@ module.exports = {
     path: path.resolve(__dirname, "build"),
     publicPath: "/cycle/",
     filename: "main.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
   }
 };
