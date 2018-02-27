b2c0c1543fe022e61321564fd1a4967d1e65e53f
Aaron Caldwell <aaronjcaldwell@gmail.com>
2018-02-27 08:12:15 -0700

    Revise cycle app to perform basic http request

--- index.js
+++ index.js
@@ -1,30 +1,49 @@
 import xs from 'xstream';
 import { run } from '@cycle/run';
-import { makeDOMDriver, div, input, p} from '@cycle/dom';
+import { div, button, h1, h4, a, makeDOMDriver } from '@cycle/dom';
+import { makeHTTPDriver } from '@cycle/http';
 
-// Analogous to 'computer' function
+// Analogous to 'computer' function, readable side effects
 function main(sources) { // 'sources' are like user event streams
-console.log(sources)
-  const sinks = {
-    DOM: sources.DOM.select('input').events('change')
-      .map(ev => ev.target.checked) 
-      .startWith(false)
-      .map( toggled => 
-        div([
-          input({attrs: {type: 'checkbox'}}), 
-          'Toggle me',
-          p(toggled ? 'ON' : 'not ON at all (OFF)')
-        ])
-      )
-  };
-  return sinks; // 'sinks' are like computer 'actuators'
+
+  // Read side effects and set up call to back-end
+  const click$ = sources.DOM.select('.get-random').events('click');
+
+  const getRandomUser$ = click$.map(() => {
+    const randomNum = Math.round(Math.random() * 9) + 1;
+    return {
+      url: `https://jsonplaceholder.typicode.com/users/${String(randomNum)}`,
+      category: 'users',
+      method: 'GET'
+    };
+  });
+
+  const user$ = sources.HTTP.select('users')
+    .flatten()
+    .map(res => res.body)
+    .startWith(null);
+
+  const vdom$ = user$.map( user => 
+    div('.users', [
+      button('.get-random', 'Get random user'),
+      user === null ? null : div('.user-details', [
+        h1('.user-name', user.name),
+        h4('.user-email', user.email),
+        a('.user-website', {href: user.website}, user.website)
+      ])
+    ])
+  );
+  // Instructions to drivers to perform side effects
+  return {
+    DOM: vdom$,
+    HTTP: getRandomUser$
+  }
 }
 
 // Adapters for the external world
 const drivers = {
-  // Takes a screen stream from computer and returns stream of
-  // mouse & keyboard events
-  DOM: makeDOMDriver('#app')
+  DOM: makeDOMDriver('#app'),
+  HTTP: makeHTTPDriver()
 }
 
 run(main, drivers);
